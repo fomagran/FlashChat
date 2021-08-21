60858ee9f65d20160ed15f74aa392368e3932b23
Fomagran <fomagran6@naver.com>
2021-08-21 19:53:56 +0900

    test commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 
+//
 void main() => runApp(FlashChat());
 
 class FlashChat extends StatelessWidget {
