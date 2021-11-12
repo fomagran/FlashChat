5e8091ad104697a001dc2697a7e0af0be796c254
Fomagran <fomagran6@naver.com>
2021-11-12 13:51:11 +0900

    [add] text animation 적용하기

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flash_chat/screens/welcome_screen.dart';
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
+import '';
 
 void main() => runApp(FlashChat());
 
